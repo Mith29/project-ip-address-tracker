8d5d4d9b1a6a038f4f5557f2c9a6327bf9159a74
Mithra <mithra29.venu@gmail.com>
2026-02-03 09:36:23 -0500

    rendered map on the webpage

--- Project/ip-address-tracker-master/script.js
+++ Project/ip-address-tracker-master/script.js
@@ -74,12 +74,12 @@ function renderAPIData(data) {
 
 //Here we create a map in the 'map' div, add tiles of our choice, and then add a marker with some text in a popup:
 
-// let map = L.map('map').setView([0, 0], 2);
+let map = L.map('map').setView([0, 0], 2);
 
-// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-// }).addTo(map);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 
-// L.marker([51.5, -0.09]).addTo(map)
-//     .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-//     .openPopup();
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+    .openPopup();
